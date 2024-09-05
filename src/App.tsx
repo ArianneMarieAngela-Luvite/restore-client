2bdd431cce493f93e5ce980135fbfb8d63403f2f
Arianne Marie Angela Luvite <ariannemarieangela.luvite@cit.edu>
2024-09-06 02:36:41 +0800

    Added temporary header file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/ui/button"
+import {ForecastGraph} from "./views/ForecastGraph"
+import Header from "./views/Header"
+import { Login } from "./views/Login"
+import ProductGraph from "./views/ProductGraph"
+import { Register } from "./views/Register"
+
+
 function App() {
   return (
     <div>
-    
+      <Header />
     </div>
   )
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+
+export const navigation = [
+    {
+        id: "0",
+        title: "About",
+        url: "#about"
+    },
+    {
+        id: "1",
+        title: "Pricing",
+        url: "#pricing"
+    },
+    {
+        id: "2",
+        title: "Contact Us",
+        url: "#contact-us"
+    }
+];
+
