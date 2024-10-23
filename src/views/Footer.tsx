c0c65fbc045c4425a41729374cb8d73fcb015b16
Arianne Marie Angela Luvite <ariannemarieangela.luvite@cit.edu>
2024-10-24 01:19:49 +0800

    Footer test

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div className="flex w-full px-10 container mx-auto border">
-        <div className="container pt-20  mx-auto">
+    <div className="flex w-[400px] justify-center items-center border-2 border-primary ">
+        <div className="container border pt-20  mx-auto">
         <div className="flex flex-col justify-center  w-full items-center gap-2 mb-10">
             <div className="font-lato font-bold text-3xl pb-5 text-center">
                 Feel connected anytime, anywhere
