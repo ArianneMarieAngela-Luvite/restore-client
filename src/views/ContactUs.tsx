a355acf398534d6a2a276ebb707956911e99be58
Arianne Marie Angela Luvite <ariannemarieangela.luvite@cit.edu>
2024-10-24 00:44:38 +0800

    Check

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -71,7 +71,7 @@ export default function ContactUs() {
           ease: "linear",
           duration: 2,
           x: { duration: 1 }
-      }} id="contact-us" className="container mx-auto  py-10 lg:px-8">
+      }} id="contact-us" className="container mx-auto border  py-10 lg:px-8">
         
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get in touch</h2>
