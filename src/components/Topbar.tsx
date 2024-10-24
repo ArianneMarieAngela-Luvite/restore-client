955afdaaa29f5c466eb976a85cd94c1d3c2ef6fb
Arianne Marie Angela Luvite <ariannemarieangela.luvite@cit.edu>
2024-10-25 01:01:59 +0800

    Logo

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -133,7 +133,7 @@ const Topbar = () => {
       <div className="flex justify-between w-screen items-center pb-4 pt-4 md:pb-7 md:pt-7 lg:py-8  shadow-md px-10 my-3 mt-0  border">
         <div className="flex items-center ">
             <div className="ml-3 mr-12 lg:mx-12 xl:mx-12">
-            <img src={restore} className="h-11 w-12" alt="Restore Logo" />
+            <img src={restore} className="h-11 w-11" alt="Restore Logo" />
             </div>
               <div className="hidden lg:block mr-10">
                 <ul className="text-customTextColor flex gap-6">
